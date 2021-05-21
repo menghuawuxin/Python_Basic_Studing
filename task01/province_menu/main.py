f798fd197ddb5c70c76ca4e70135a8bf2643244a
ing <lztts1998@qq.com>
2021-05-21 08:30:19 +0800

    修改README，增加province_menu项目

---
+++ task01/province_menu/main.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+# encoding: utf-8
+
+
+def basic_data():
+    '''
+    省市区信息
+    :return:
+    '''
+    urban = {"江苏省": {"南通市", "苏州市"},
+             "甘肃省": {"酒泉市", "张掖市"},
+             "南通市": {"崇川区", "通州区"},
+             "苏州市": {"虎丘区", "吴中区"},
+             "酒泉市": {"肃州区", "金塔县"},
+             "张掖市": {"甘州区", "民乐县"}}
+    pass
+
+
+def find_data():
+    '''
+    寻找下级菜单
+    :return:
+    '''
+    pass
+
+if __name__ == '__main__':
+    params = find_data()
