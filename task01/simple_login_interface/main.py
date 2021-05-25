b49a0d62b99aa4c75d3f565b82139724cc2f6db8
ing <lztts1998@qq.com>
2021-05-25 20:08:17 +0800

    封装函数调整代码规范

--- task01/simple_login_interface/main.py
+++ task01/simple_login_interface/main.py
@@ -6,25 +6,58 @@ import pandas as pd
 
 
 def get_login_information():
+    """
+    读取表中存取的用户名密码
+    :return:
+    """
     get_data = pd.read_csv("basic_information", sep=" ")
     deal_data = get_data.groupby("username")["password"].apply(list).to_dict()
     return deal_data
 
 
-def login(username, password, data):
-    '''
-    判断输入的用户名密码是否正确
+def locking(username):
+    """
+    将要锁定的用户写入表
     :param username:
-    :param password:
     :return:
-    '''
+    """
+    with open("locking_user", mode='a+', encoding="utf-8") as w:
+        w.write("\n" + username)
+    return None
 
-    locked_sige = get_locking_name(username)
+
+def get_locking_name(username):
+    """
+    获取锁定的用户名
+    :param username:
+    :return:
+    """
+    locking_name = open("locking_user", mode='r', encoding="utf-8").read().split("\n")
+    flag = False
+    for locked_name in locking_name:
+        if username == locked_name:
+            flag = True
+    return flag
+
+
+def login_judge_locked(locked_sige, username, password):
+    """
+    判断用户是否被锁定
+    :return:
+    """
     while locked_sige:
         print("该用户已锁定")
         username = input("请重新输入用户名：")
         password = input("请重新输入密码：")
         locked_sige = get_locking_name(username)
+    return username, password
+
+
+def login_judge_username(username, password):
+    """
+    判断用户名是否正确
+    :return:
+    """
     flag = False
     data_name = data.keys()
     while not flag:
@@ -34,6 +67,14 @@ def login(username, password, data):
         if not flag:
             username = input("无此用户，请重新输入用户名：")
             password = input("请重新输入密码：")
+    return flag, username, password
+
+
+def login_judge_password(flag, username, password):
+    """
+    判断密码是否正确, 多次错误锁定该用户
+    :return:
+    """
     if flag and data[username][0] == password:
         print("欢迎登陆")
         sys.exit()
@@ -48,27 +89,26 @@ def login(username, password, data):
         locking(username)
         username = input("密码多次错误，该用户被锁定，请重新输入用户名：")
         password = input("请输入密码：")
-        login(username, password, data)
-        return flag
+        login(username, password)
+    return flag, username, password
 
 
-def locking(username):
-    with open("locking_user", mode='a+', encoding="utf-8") as w:
-        w.write("\n" + username)
+def login(username, password):
+    """
+    判断输入的用户名密码是否正确
+    :param username:
+    :param password:
+    :return:
+    """
+    locked_sige = get_locking_name(username)
+    username, password = login_judge_locked(locked_sige, username, password)
+    flag, username, password = login_judge_username(username, password)
+    login_judge_password(flag, username, password)
     return None
-
-
-def get_locking_name(username):
-    locking_name = open("locking_user", mode='r', encoding="utf-8").read().split("\n")
-    flag = False
-    for locked_name in locking_name:
-        if username == locked_name:
-            flag = True
-    return flag
 
 
 if __name__ == '__main__':
     data = get_login_information()
     get_name = input("请输入用户名；")
     get_password = input("请输入密码：")
-    get_login_data = login(get_name, get_password, data)
+    login(get_name, get_password)
